67f8b43c8cf795b23d20dc1bfe81bbe4a448b49e
abdullah4545 <md.muraiem@gmail.com>
2023-05-23 02:13:23 +0600

    f

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -49,7 +49,7 @@ class NewsController extends Controller
 
     public function getnewsbyid(Request $request, $slug)
     {
-        return response()->json($request, 200);
+        return $token = $request->header('Authorization');
         $news =Newsupdate::where('slug',$request->slug)->where('status','Active')->first();
         $news->postImage=env('PROD_URL').$news->postImage;
 
