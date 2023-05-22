4805f4f8bca80ad8ae64a6fe37c45fe665598970
abdullah4545 <md.muraiem@gmail.com>
2023-05-23 02:24:47 +0600

    d

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Newsupdate;
 use DB;
+use Laravel\Sanctum\PersonalAccessToken;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Seennewsupdate;
 
