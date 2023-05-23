a1bbfa62dcf9a26632daaae8d6aa13abeb4772bf
abdullah4545 <md.muraiem@gmail.com>
2023-05-23 20:56:39 +0600

    d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //  $this->call(UserSeeder::class);
-        //  $this->call(RolePermissionSeeder::class);
-        //  $this->call(UserRolePermissionSeeder::class);
-        //  $this->call(BasicinfoSeeder::class);
-        //  $this->call(AdminSeeder::class);
-        //  $this->call(AccounttypeSeeder::class);
-        //  $this->call(AccountpackageSeeder::class);
-        //  $this->call(NewsupdateSeeder::class);
+         $this->call(RolePermissionSeeder::class);
+         $this->call(UserRolePermissionSeeder::class);
+         $this->call(BasicinfoSeeder::class);
+         $this->call(AdminSeeder::class);
+         $this->call(AccounttypeSeeder::class);
+         $this->call(AccountpackageSeeder::class);
+         $this->call(NewsupdateSeeder::class);
          $this->call(AboutusSeeder::class);
          $this->call(HelpcenterSeeder::class);
          $this->call(TeammemberSeeder::class);
