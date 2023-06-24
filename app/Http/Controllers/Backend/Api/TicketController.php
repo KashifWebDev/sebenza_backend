34292ade9b999f63b829c747ea98801024d031c8
abdullah4545 <md.muraiem@gmail.com>
2023-06-24 10:25:19 +0600

    d

--- app/Http/Controllers/Backend/Api/TicketController.php
+++ app/Http/Controllers/Backend/Api/TicketController.php
@@ -24,7 +24,7 @@ class TicketController extends Controller
         $token = request()->bearerToken();
         if(isset($token)){
             $user_id=PersonalAccessToken::findToken($token);
-          	$tickets=Ticket::with('users')->where('from_id',$user_id->tokenable_id)->get();
+          	$tickets=Ticket::with('users')->get();
         	$response = [
                 'status' => true,
                 'message'=>'Supporttickets by user id',
