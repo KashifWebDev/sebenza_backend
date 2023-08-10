aa2b772327408deb1e7d8e0b40e4aeff0c842cde
abdullah4545 <md.muraiem@gmail.com>
2023-08-10 23:59:58 +0600

    f

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -16,27 +16,19 @@ class UserImport implements ToModel
     public function model(array $row)
     {
         $email=User::where('email', $row[0])->first();
-        $phonenumber=User::where('phone', $row[0])->first();
         if($email){
 
-        }elseif($phonenumber){
-
         }else{
+            $token = request()->bearerToken();
+            $user_id=PersonalAccessToken::findToken($token);
+            $memberof=User::where('id', $user_id->tokenable_id)->first();
 
-
-            return new User([
-                'first_name'=>$row[1],
-                'last_name'=>$row[2],
-                'phone'=>$row[3],
-                'email'=>$row[4],
-                'password'=>Hash::make($row[9]),
-                'member_by'=>$row[0],
-                'company_name'=>$row[10],
-                'country'=>$row[5],
-                'city'=>$row[6],
-                'address'=>$row[7],
-            ]);
-
+            $user=new User();
+            $user->email=$row[0];
+            $user->member_by=$memberof->membership_code;
+            $user->assignRole('user');
+            $user->save();
+            return $user;
         }
 
     }
