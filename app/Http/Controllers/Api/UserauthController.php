dc7b92404afb7d83394d8a78aaaa9eba646f6f39
abdullah4545 <md.muraiem@gmail.com>
2023-08-11 00:28:27 +0600

    f

--- app/Http/Controllers/Api/UserauthController.php
+++ app/Http/Controllers/Api/UserauthController.php
@@ -27,7 +27,7 @@ class UserauthController extends Controller
         $memberof=User::where('id', $user_id->tokenable_id)->first();
         $count=User::where('member_by', $memberof->membership_code)->get()->count();
 
-        $totalRows = count(\Excel::import($request->file, function($reader) {})->get());;
+        $totalRows = count(\Excel::load($request->file->getPathName(), function($reader) {})->get());;
 
         if($count<$memberof->user_limit_id){
             if($totalRows<($memberof->user_limit_id-$count)){
