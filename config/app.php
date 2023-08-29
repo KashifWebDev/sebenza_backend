3d7c70e316501822da2311b5f710cae726ea1bc6
abdullah4545 <md.muraiem@gmail.com>
2023-08-29 11:35:20 +0600

    f

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         Spatie\Permission\PermissionServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Srmklive\PayPal\Providers\PayPalServiceProvider::class,
         /*
          * Application Service Providers...
          */
