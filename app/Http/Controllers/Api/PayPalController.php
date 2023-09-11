0db85843d8e886ca6b69933e2f938c7df465fd7e
abdullah4545 <md.muraiem@gmail.com>
2023-09-11 23:43:12 +0600

    g

--- app/Http/Controllers/Api/PayPalController.php
+++ app/Http/Controllers/Api/PayPalController.php
@@ -88,7 +88,7 @@ class PayPalController extends Controller
 
     public function paymentSuccess(Request $request)
     {
-        return $request;
+        $response=json_decode($request->successResponse);
         if (isset($response['status']) && $response['status'] == 'COMPLETED') {
             $invoice =Invoice::where('payment_id',$response['id'])->first();
             $invoice->payment_id=$response['id'];
