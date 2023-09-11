cdf07a3269da40e83213fb1818f01c8f5ae7b3dd
abdullah4545 <md.muraiem@gmail.com>
2023-09-11 22:05:22 +0600

    f

--- app/Http/Controllers/Api/PayPalController.php
+++ app/Http/Controllers/Api/PayPalController.php
@@ -4,7 +4,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
