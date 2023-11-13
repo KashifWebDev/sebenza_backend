9d09a3267af9fbd4fb97c5cae2cb16b58634b86c
abdullah4545 <md.muraiem@gmail.com>
2023-11-14 00:55:54 +0600

    f

--- app/Exports/SaleExport.php
+++ app/Exports/SaleExport.php
@@ -48,7 +48,7 @@ class SaleExport implements FromQuery,WithHeadings,WithMapping
     {
         $startDate=$this->startDate;
         $endDate=$this->endDate;
-        return Sale::with(['saleitems'])->where('orderDate', $startDate);
+        return Sale::whereBetween('orderDate', [$startDate, $endDate]);
     }
 
 
