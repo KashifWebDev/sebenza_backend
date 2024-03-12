136877407978b11ad0f2335f1641cd6e33d066f4
abdullah4545 <md.muraiem@gmail.com>
2024-03-13 02:08:41 +0600

    f

--- app/Http/Controllers/Backend/Api/WhatsappController.php
+++ app/Http/Controllers/Backend/Api/WhatsappController.php
@@ -16,11 +16,7 @@ class WhatsappController extends Controller
     public function ipinfo(){
         $positions = GeoLocation::lookup('103.49.203.178');
 
-        $amount=Currency::convert()
-            ->from('USD')
-            ->to('BDT')
-            ->amount(100)
-            ->get();
+        return Currency::convert()->from('USD')->to('BDT')->amount(100)->get();
 
         return 'gi';
     }
